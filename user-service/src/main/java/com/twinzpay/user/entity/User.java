a92ec0c7aa95189a4630300ba7a62171bee23ced
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-21 14:23:38 +0100

    Updated the User entity

--- user-service/src/main/java/com/twinzpay/user/entity/User.java
+++ user-service/src/main/java/com/twinzpay/user/entity/User.java
@@ -8,31 +8,30 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "user_table")
 @Getter
 @Setter
-@Table(name = "user_table")
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    private String email;
-
     @Column(nullable = false)
     private String fullName;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
 
-    private String password; // We will encrypt this later
+    @Column(nullable = false)
+    private String password; // We will encrypt this before saving!
 
     @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 }
