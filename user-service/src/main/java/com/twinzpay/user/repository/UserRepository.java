a2f2c1ff61fd3ffa15cda24185688e0c691bb8c5
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-21 14:25:40 +0100

    Updated UserRepository interface to contain more method declarations

--- user-service/src/main/java/com/twinzpay/user/repository/UserRepository.java
+++ user-service/src/main/java/com/twinzpay/user/repository/UserRepository.java
@@ -2,10 +2,17 @@ package com.twinzpay.user.repository;
 
 import com.twinzpay.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // Used for login
     Optional<User> findByEmail(String email);
+
+    // Used for registration validation
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
