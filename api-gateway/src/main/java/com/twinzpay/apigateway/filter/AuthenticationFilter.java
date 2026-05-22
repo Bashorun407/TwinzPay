6446f0ba50728a02afaf8cf0bac2f21157458d5c
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-22 14:06:15 +0100

    Created Authentication Filter class in api gateway's filter package

---
+++ api-gateway/src/main/java/com/twinzpay/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.twinzpay.apigateway.filter;
+
+import com.twinzpay.apigateway.util.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final RouteValidator validator;
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationFilter(RouteValidator validator, JwtUtil jwtUtil) {
+        super(Config.class);
+        this.validator = validator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+
+                // 1. Check if the Authorization header exists
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+
+                // 2. Extract the token
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                } else {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+
+                // 3. Validate the token
+                try {
+                    jwtUtil.validateToken(authHeader);
+                } catch (Exception e) {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+        // Empty class required by Spring Cloud Gateway architecture
+    }
+}
