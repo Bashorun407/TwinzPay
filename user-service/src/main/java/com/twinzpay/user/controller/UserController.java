cd1a5864189af17aa7d62c78f06db15029bf73a0
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-22 11:44:08 +0100

    Created and successfully tested UserController class through API gateway

---
+++ user-service/src/main/java/com/twinzpay/user/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.twinzpay.user.controller;
+
+import com.twinzpay.user.dto.LoginRequestDto;
+import com.twinzpay.user.dto.RegisterRequestDto;
+import com.twinzpay.user.dto.UserResponseDto;
+import com.twinzpay.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> register(@RequestBody RegisterRequestDto request) {
+        UserResponseDto response = userService.registerUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDto> login(@RequestBody LoginRequestDto request) {
+        UserResponseDto response = userService.loginUser(request);
+        return ResponseEntity.ok(response);
+    }
+}
