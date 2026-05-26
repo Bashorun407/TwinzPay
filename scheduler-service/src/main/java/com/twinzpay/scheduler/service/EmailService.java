2b6229083c6fdc96dcfc054794deaaee68f6e0a8
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-26 18:58:13 +0100

    Created an Email Service in the Schedule Service package

---
+++ scheduler-service/src/main/java/com/twinzpay/scheduler/service/EmailService.java
@@ -0,0 +1,33 @@
+package com.twinzpay.scheduler.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendPaymentWarning(String toEmail, String timeRemaining, String amount, String account) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("automation@twinzpay.com");
+        message.setTo(toEmail);
+        message.setSubject("Upcoming Payment Reminder: " + timeRemaining);
+
+        String body = String.format(
+                "Hello,\n\n" +
+                        "This is a reminder that your scheduled automated payment of NGN %s for account/meter %s " +
+                        "will be processed in exactly %s.\n\n" +
+                        "If you wish to cancel or modify this payment, please log into your TwinzPay dashboard immediately.\n\n" +
+                        "Thank you,\nTwinzPay Automation Team",
+                amount, account, timeRemaining
+        );
+
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
