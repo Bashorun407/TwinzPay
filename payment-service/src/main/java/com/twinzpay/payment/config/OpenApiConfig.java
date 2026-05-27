87cb9a3cfd0c794ac71e0022a9a99366473c32bb
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-05-27 19:41:51 +0100

    Created OpenApiConfig class in config package for all services to point rquests to API gateway seamlessly

---
+++ payment-service/src/main/java/com/twinzpay/payment/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.twinzpay.payment.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "API Gateway")
+        }
+)
+public class OpenApiConfig {
+}
