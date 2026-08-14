69569c25793e2866d77cf954d4e4891da9ac862b
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-08-14 09:50:36 +0100

    Disabled validations on AutoChargeRequest DTO because it is a PayStack-to-TwinzPay DTO

--- payment-service/src/main/java/com/twinzpay/payment/dto/AutoChargeRequest.java
+++ payment-service/src/main/java/com/twinzpay/payment/dto/AutoChargeRequest.java
@@ -5,14 +5,14 @@ import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 
 public record AutoChargeRequest(
-        @NotBlank(message = "Email is required")
-        @Email(message = "Please provide a valid email address")
+//        @NotBlank(message = "Email is required")
+//        @Email(message = "Please provide a valid email address")
         String userEmail,
-        @NotBlank(message = "Amount is required")
-        @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
+//        @NotBlank(message = "Amount is required")
+//        @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
         //Alternatively use @Digits(integer = 10, fraction = 2) to enforce format
         BigDecimal amount,
-        @Positive(message = "Bill Plan ID must be a positive number")
+//        @Positive(message = "Bill Plan ID must be a positive number")
         Long billPlanId
 ) {
 }
